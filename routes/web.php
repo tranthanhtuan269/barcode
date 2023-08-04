0bb0fdd34a72baa9bcfd9caaa3998f90f94cd075
tranthanhtuan269 <tran.thanh.tuan269@gmail.com>
2023-08-04 17:15:21 +0700

    css

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\RedirectController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StatisticalController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RssController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('tuantt', [HomeController::class, 'tuantt']);
