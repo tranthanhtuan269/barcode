e3f0e163bd8f66be1bb7b1cfafae1c5e3a085dd5
tranthanhtuan269 <tran.thanh.tuan269@gmail.com>
2023-08-07 17:28:42 +0700

    css

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -515,7 +515,7 @@ class PublicController extends Controller
 			if($data && $data->show_status == 1){
 				$data->view_count ++;
 				$data->save();
-				if($data->related_articles) {
+				if($data->related_articles != "null") {
 					$related_articles = Article::whereIn('id', json_decode($data->related_articles, TRUE))->orderBy('count_view', 'desc')->get();
 				}else {
 					$related_articles = Article::orderBy('count_view', 'desc')->take(10)->get();
