39d51ed2312ca64a92222bdc51e922b3e97574de
tranthanhtuan269 <tran.thanh.tuan269@gmail.com>
2023-08-11 16:51:41 +0700

    css

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->removeIndexPHPFromURL();
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
@@ -36,5 +38,25 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
+    }
+
+    protected function removeIndexPHPFromURL()
+    {
+        if (Str::contains(request()->getRequestUri(), '/index.php/')) {
+            $url = str_replace('/index.php/', '/', request()->getRequestUri());
+  
+            if (strlen($url) > 0) {
+                header("Location: $url", true, 301);
+                exit;
+            }
+        }
+        if (Str::contains(request()->getRequestUri(), '/index.php')) {
+            $url = str_replace('/index.php', '/', request()->getRequestUri());
+  
+            if (strlen($url) > 0) {
+                header("Location: /", true, 301);
+                exit;
+            }
+        }
     }
 }
